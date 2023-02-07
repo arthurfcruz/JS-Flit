711a581921d87625179e652af28546ac96813a22
Arthur <arthurfelixcruz@gmail.com>
2023-02-07 20:49:25 -0300

    1.3 parte 1

--- teste.js
+++ teste.js
@@ -1,3 +1,19 @@
-caminho_5km && corro_2km, passo_o_dia_bem
-levanto_cedo || antes_das_8h, saio_tranquilo
-!termino_o_trabalho || !tenho_compromisso, saio_tarde
+var caminho = '5Km'
+var corro = '2Km'
+
+if(caminho === '5Km' && corro === '2Km'){
+    console.log("Passo o dia bem.")
+}
+
+var levanto = 'antes das 8'
+
+if(levanto === 'cedo' || levanto === 'antes das 8'){
+    console.log("Saio tranquilo.")
+}
+
+var termino = 'o trabalho'
+var tenho = 'compromisso'
+
+if(!termino === 'o trabalho' || !tenho === 'compromisso'){
+    console.log("Saio tarde.")
+}
