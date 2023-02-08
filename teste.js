79448a7f6392ea6ba7b116d29d025925fd4b96f5
Arthur <arthurfelixcruz@gmail.com>
2023-02-07 22:34:59 -0300

    1.3 parte 2.1

--- teste.js
+++ teste.js
@@ -1,19 +1,44 @@
-var caminho = '5Km'
-var corro = '2Km'
+// var caminho = '5Km'
+// var corro = '2Km'
 
-if(caminho === '5Km' && corro === '2Km'){
-    console.log("Passo o dia bem.")
+// if(caminho === '5Km' && corro === '2Km'){
+//     console.log("Passo o dia bem.")
+// }
+
+// var levanto = 'antes das 8'
+
+// if(levanto === 'cedo' || levanto === 'antes das 8'){
+//     console.log("Saio tranquilo.")
+// }
+
+// var termino = 'o trabalho'
+// var tenho = 'compromisso'
+
+// if(!termino === 'o trabalho' || !tenho === 'compromisso'){
+//     console.log("Saio tarde.")
+// }
+
+var numero = 5
+var contador = 0
+
+for (let index = 0; index <= numero; index++) {
+   switch (numero % index) {
+        case 0:
+            contador ++
+        break;
+   
+        default:
+        break;
+    }
 }
 
-var levanto = 'antes das 8'
+switch (contador) {
+    case 2:
+        console.log("o número", numero, "é primo") 
+    break;
 
-if(levanto === 'cedo' || levanto === 'antes das 8'){
-    console.log("Saio tranquilo.")
+    default:
+        console.log("o número", numero, "nao é primo")
+    break;
 }
-
-var termino = 'o trabalho'
-var tenho = 'compromisso'
-
-if(!termino === 'o trabalho' || !tenho === 'compromisso'){
-    console.log("Saio tarde.")
-}
+ 
