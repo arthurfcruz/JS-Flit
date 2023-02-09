6fa286e5abb777001b6c0a850c33658d3c724b8f
Arthur <arthurfelixcruz@gmail.com>
2023-02-08 21:37:59 -0300

    1.4 (1)

--- teste.js
+++ teste.js
@@ -1,47 +1,7 @@
-// var caminho = '5Km'
-// var corro = '2Km'
+var array = [12,6,9,42,11,102,44,25]
+var soma = 0
 
-// if(caminho === '5Km' && corro === '2Km'){
-//     console.log("Passo o dia bem.")
-// }
-
-// var levanto = 'antes das 8'
-
-// if(levanto === 'cedo' || levanto === 'antes das 8'){
-//     console.log("Saio tranquilo.")
-// }
-
-// var termino = 'o trabalho'
-// var tenho = 'compromisso'
-
-// if(!termino === 'o trabalho' || !tenho === 'compromisso'){
-//     console.log("Saio tarde.")
-// }
-
-// var numero = 5
-// var contador = 0
-
-// for (let index = 0; index <= numero; index++) {
-//    switch (numero % index) {
-//         case 0:
-//             contador ++
-//         break;
-   
-//         default:
-//         break;
-//     }
-// }
-
-// switch (contador) {
-//     case 2:
-//         console.log("o número", numero, "é primo") 
-//     break;
-
-//     default:
-//         console.log("o número", numero, "nao é primo")
-//     break;
-// }
-
-var hora = 1
-var minuto = 0
-hora > 0 ? minuto = hora * 60 : minuto = 60
+for (let index = 0; index < array.length; index++) {
+    soma = soma + array[index]
+    console.log(soma)
+}
