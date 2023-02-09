398c1d9a67d16fc338d2c9744d5bff55eac92c16
Arthur <arthurfelixcruz@gmail.com>
2023-02-09 20:49:50 -0300

    1.5.2

--- teste.js
+++ teste.js
@@ -1,7 +1,22 @@
+//1.5 - 1
+
 var nome = ""
 var senha = ""
 
 function Cadastro() {
     var nome = prompt("Digite seu nome")
     var senha = prompt("Digite sua senha")
+}
+
+//1.5 - 2
+
+function SolicitarDados(nome,senha) {
+    var nome2 = prompt("Digite seu nome")
+    var senha2 = prompt("Digite sua senha")
+
+    if (nome2 == nome && senha2 == senha) {
+        return true
+    }else{
+        return false
+    } 
 }
