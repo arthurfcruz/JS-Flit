c045b727673317980cdad97ca4aa49c6dce9c10d
Arthur <arthurfelixcruz@gmail.com>
2023-02-08 22:18:38 -0300

    1.4 (completo)

--- teste.js
+++ teste.js
@@ -6,9 +6,29 @@
 //     console.log(soma)
 // }
 
-for (let i = 0; i < 90; i++) {
-    while ((i < 40 || i > 50) && i < 90) {
-        console.log(i)
-        i++
+// for (let i = 0; i < 90; i++) {
+//     while ((i < 40 || i > 50) && i < 90) {
+//         console.log(i)
+//         i++
+//     }
+// }
+
+var numero = 0
+var contador = 0
+var primo = false
+
+while (primo == false) {
+    for (let index = 0; index <= numero; index++) {
+        if (numero % index == 0) {
+            contador ++
+        }
     }
+    
+    if (contador == 2) {
+        primo = true
+    }else{
+        contador = 0
+        numero ++
+    }
+    console.log(numero)
 }
