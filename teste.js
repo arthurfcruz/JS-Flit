548ab64bd2e9d86b14caad8eaaf8eb94acf4664e
Arthur <arthurfelixcruz@gmail.com>
2023-02-08 21:58:58 -0300

    1.4 (2.2)

--- teste.js
+++ teste.js
@@ -1,7 +1,14 @@
-var array = [12,6,9,42,11,102,44,25]
-var soma = 0
+// var array = [12,6,9,42,11,102,44,25]
+// var soma = 0
 
-for (let index = 0; index < array.length; index++) {
-    soma = soma + array[index]
-    console.log(soma)
+// for (let index = 0; index < array.length; index++) {
+//     soma = soma + array[index]
+//     console.log(soma)
+// }
+
+for (let i = 0; i < 90; i++) {
+    while ((i < 40 || i > 50) && i < 90) {
+        console.log(i)
+        i++
+    }
 }
